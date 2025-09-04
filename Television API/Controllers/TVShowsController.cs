50b3d8cb4400ae67770fdc6af285f97cbb82b7cd
tdteixeira <166443420+tdteixeira@users.noreply.github.com>
2025-09-04 14:23:37 +0100

    Add project files.

---
+++ Television API/Controllers/TVShowsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Television_API.Models;
+
+namespace Television_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TVShowsController : ControllerBase
+    {
+
+        private readonly ILogger<TVShowsController> _logger;
+
+        public TVShowsController(ILogger<TVShowsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetTVShows")]
+        public IEnumerable<TVShow> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new TVShow
+            {
+                Id = index,
+                Title = $"TV Show {index}",
+                Genre = $"Genre {index}",
+                StartDate = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                IsOngoing = index % 2 == 0
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Television API/Models/TVShow.cs
@@ -0,0 +1,11 @@
+﻿namespace Television_API.Models
+{
+    public class TVShow
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Genre { get; set; }
+        public DateOnly StartDate { get; set; }
+        public bool IsOngoing { get; set; }
+    }
+}
