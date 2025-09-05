c291a262eb580c1f1caf2da31d0604b5c4368093
tdteixeira <166443420+tdteixeira@users.noreply.github.com>
2025-09-05 19:57:23 +0100

    Create ActorsController.cs

---
+++ Television API/Controllers/ActorsController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Television_API.Data;
+using Television_API.Models;
+
+namespace Television_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActorsController: ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ActorsController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet(Name = "GetActors")]
+        public async Task<IEnumerable<ActorDto>> GetActors()
+        {
+            var actors = await _context.Actors
+                .ToListAsync();
+            return _mapper.Map<List<ActorDto>>(actors);
+        }
+
+    }
+}
