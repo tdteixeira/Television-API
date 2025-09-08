13125097d5c80eed5682c3e540b02eb0c290cf4b
tdteixeira <166443420+tdteixeira@users.noreply.github.com>
2025-09-08 22:32:54 +0100

    Added Documentation for TVShow Controller

--- Television API/Controllers/TVShowsController.cs
+++ Television API/Controllers/TVShowsController.cs
@@ -17,6 +17,11 @@ namespace Television_API.Controllers
             _repository = repository;
         }
 
+        /// <summary>
+        /// Retrieves a paginated list of all TV shows.
+        /// </summary>
+        /// <param name="p">Pagination parameters - page number and page size.</param>
+        /// <returns>A list of <see cref="TVShowDto"/> objects.</returns>
         [HttpGet()]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(IEnumerable<TVShowDto>))]
         public async Task<IActionResult> GetTVShows([FromQuery] PaginationParams p)
@@ -25,6 +30,11 @@ namespace Television_API.Controllers
             return Ok(tvshows);
         }
 
+        /// <summary>
+        /// Retrieves all episodes for a specific TV show.
+        /// </summary>
+        /// <param name="showId">The ID of the TV show.</param>
+        /// <returns>A list of <see cref="EpisodeDto"/> objects.</returns>
         [HttpGet("{showId:int}/episodes")]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(IEnumerable<EpisodeDto>))]
         public async Task<IActionResult> GetTVShowEpisodes(int showId)
@@ -33,6 +43,11 @@ namespace Television_API.Controllers
             return Ok(episodes);
         }
 
+        /// <summary>
+        /// Retrieves all actors associated with a specific TV show.
+        /// </summary>
+        /// <param name="showId">The ID of the TV show.</param>
+        /// <returns>A list of <see cref="ActorDto"/> objects.</returns>
         [HttpGet("{showId:int}/actors")]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(IEnumerable<ActorDto>))]
         public async Task<IActionResult> GetTVShowActors(int showId)
@@ -41,6 +56,12 @@ namespace Television_API.Controllers
             return Ok(actors);
         }
 
+        /// <summary>
+        /// Searches TV shows using filter criteria and returns a paginated result.
+        /// </summary>
+        /// <param name="p">Pagination parameters - page number and page size.</param>
+        /// <param name="dto">Filter criteria - title, genres, status, startDate, id.</param>
+        /// <returns>A filtered list of <see cref="TVShowDto"/> objects.</returns>
         [HttpPost("search")]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(IEnumerable<TVShowDto>))]
         public async Task<IActionResult> SearchTVShows([FromQuery] PaginationParams p, [FromQuery] TVShowDto dto)
